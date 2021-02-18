e6b433f633b9930212c3a3678b8502d0fe6516bc
Karan Vishwakarma <karan.vish17@gmail.com>
2021-02-17 22:20:15 -0600

    front layout explore

--- Code/gps-trails/src/app/explore/explore.component.ts
+++ Code/gps-trails/src/app/explore/explore.component.ts
@@ -15,4 +15,16 @@ export class ExploreComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  parklist: { id: number, parkname: string, imgurl: string, parkDesc: string }[] = [
+    {
+      "id": 0, parkname: "Yosemite National Park",
+      imgurl: "https://www.nationalgeographic.com/content/dam/travel/2019-digital/yosemite-guide/yosemite-national-park-california.jpg"
+      , parkDesc: "Beautiful destination in the world attracts speople all round the world"
+    },
+    { "id": 1, parkname: "Bay area national", imgurl: "https://www.apple.com/newsroom/images/product/services/lifestyle/ApplePay-and-AppleWatch-celebrate-americas-national-parks-08232018_big.jpg.large.jpg", parkDesc: "Located in the Silicon valley of United States.." },
+    { "id": 2, parkname: "Arizona reserves", imgurl: "https://studybreaks.com/wp-content/uploads/2017/07/shutterstock_142351951.jpg", parkDesc: "Beautiful destination in the world attracts speople all round the world" },
+    { "id": 3, parkname: "Carmel by Sea", imgurl: "https://news.harvard.edu/wp-content/uploads/2019/09/leo-serrat_unsplash-1-1600x900.jpg", parkDesc: "Beautiful destination in the world attracts speople all round the world" },
+
+
+  ];
 }
