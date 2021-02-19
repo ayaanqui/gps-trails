4b2de2ad51c480e65b488c72f56a185e540f8cf0
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-02-19 13:53:18 -0600

    Implement stars generation

--- Code/gps-trails/src/app/trail-brief/trail-brief.component.ts
+++ Code/gps-trails/src/app/trail-brief/trail-brief.component.ts
@@ -9,6 +9,21 @@ export class TrailBriefComponent implements OnInit {
 
   @Input() name: string = "";
   @Input() description: string = "";
+  @Input() views: string = "";
+  @Input() ratingsAvg: string = "";
+  
+  getStars: Function = () => {
+    let stars = "";
+    const ratings: number = parseFloat(this.ratingsAvg);
+    const leftovers: number = 5-ratings;
+
+    for (let i = 0; i < ratings; i++)
+      stars += '<i class="fa fa-star"></i>\n';
+
+    for (let i = 0; i < leftovers-1; i++)
+      stars += '<i class="fa fa-star-o"></i>\n';
+    return stars;
+  };
 
   constructor() { }
 
