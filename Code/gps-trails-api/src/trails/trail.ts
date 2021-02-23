e4bb404f4abb3ca22cf7c4d9cbafc5cef2d6ffbf
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-02-23 14:00:06 -0600

    Add Trail class

--- Code/gps-trails-api/src/trails/trail.ts
+++ Code/gps-trails-api/src/trails/trail.ts
@@ -1,17 +1,29 @@
 export class Trail {
     id: number; 
     name: string; 
-    description: string; 
     image: string; 
+    description: string; 
     ratingsAvg: number; 
     views: number;
+    lon: number;
+    lat: number;
 
-    constructor(id: number, name: string, description: string, image: string, ratingesAvg: number, views: number) {
+    constructor(id: number, name: string, image: string, description: string) {
         this.id = id;
         this.name = name;
-        this.description = description;
         this.image = image;
-        this.ratingsAvg = ratingesAvg;
-        this.views = views;
+        this.description = description;
+        this.ratingsAvg = this.genRandomDec(0, 5, 1);
+        this.views = this.genRandom(231, 14841131);
+        this.lat = this.genRandomDec(-180, 180, 3);
+        this.lon = this.genRandomDec(-180, 180, 3);
+    }
+
+    private genRandom(min: number, max:number): number {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    private genRandomDec(from: number, to: number, fixed: number): number {
+        return Number.parseFloat((Math.random() * (to - from) + from).toFixed(fixed)) * 1;
     }
 };
