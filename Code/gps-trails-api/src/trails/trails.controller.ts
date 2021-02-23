98d112586249b6c6f108f190fb529efefc7557c4
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-02-23 14:00:21 -0600

    Add trails controller

--- Code/gps-trails-api/src/trails/trails.controller.ts
+++ Code/gps-trails-api/src/trails/trails.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from "@nestjs/common";
 import { Trail } from "./trail";
+import { data } from "./trails.json";
 
 @Controller('trails')
 export class TrailsController {
     
+    trails: Array<Trail> = new Array<Trail>();
+
+    constructor() {
+        data.forEach((trail, i) => {
+            this.trails.push(new Trail(i, trail.name, trail.image, trail.description));
+        });
+    }
+
     @Get()
     getAllTrails(): Trail[] {
-        return [];
+        return this.trails;
     }
 
 }
