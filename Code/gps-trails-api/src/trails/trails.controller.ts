25059c9eb96dd18ed53609ab3a2e8044a51a20bd
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-02-23 14:24:23 -0600

    Add /trails/:id route

--- Code/gps-trails-api/src/trails/trails.controller.ts
+++ Code/gps-trails-api/src/trails/trails.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, HttpException, HttpStatus, Param } from "@nestjs/common";
 import { Trail } from "./trail";
 import { data } from "./trails.json";
 
@@ -16,6 +16,18 @@ export class TrailsController {
     @Get()
     getAllTrails(): Trail[] {
         return this.trails;
+    }
+
+    @Get(':id')
+    getTrailById(@Param() params): Trail {
+        const id: number = params?.id;
+        if (id == null || id >= this.trails.length)
+            throw new HttpException({
+                status: HttpStatus.NOT_FOUND,
+                error: 'Trail not found',
+            }, 
+            HttpStatus.NOT_FOUND);
+        return this.trails[id];
     }
 
 }
