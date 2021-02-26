92bb81a38cec5789d573651c856546c2884a7210
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-02-25 19:31:11 -0600

    Remove db config from app.module.ts

--- Code/gps-trails-api/src/app.module.ts
+++ Code/gps-trails-api/src/app.module.ts
@@ -9,16 +9,7 @@ import { UsersService } from './users/users.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Blue420Sky',
-      database: 'NestJSUserDB',
-      entities: [User],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(),
     UsersModule,
     UsersService,
     TypeOrmModule.forFeature([User],),
