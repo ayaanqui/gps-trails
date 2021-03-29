7c19f71a946b053d064d0e4d1f8348c3dc952bff
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-03-28 21:08:13 -0500

    Add local auth guard

--- Code/gps-trails-api/src/auth/auth.controller.ts
+++ Code/gps-trails-api/src/auth/auth.controller.ts
@@ -4,12 +4,13 @@ import { AuthGuard } from '@nestjs/passport';
 import { AuthUserDto } from "src/users/dto/auth-user.dto";
 import { User } from 'src/users/users.entity';
 import { UsersService } from "src/users/users.service";
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly userServices: UsersService) {}
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async postLogin(@Request() req): Promise<any> {
     return req.user;
