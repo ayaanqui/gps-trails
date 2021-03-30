16583b8111137da8f5f144167dd44dd256406d9e
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-03-29 21:13:48 -0500

    Connect with /auth/login api

--- Code/gps-trails/src/app/account/login/login.component.ts
+++ Code/gps-trails/src/app/account/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string = "";
+  password: string = "";
+
+  errorMsg: string = "";
+  accessToken: string = "";
+
+  error: boolean = false;
+  success: boolean = false;
+
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.httpClient.post<any>('http://localhost:3000/auth/login', {
+        email: this.email,
+        password: this.password
+      }
+    )
+      .subscribe(
+        (data: {access_token: string}) => {
+          this.error = false;
+          this.success = true;
+          this.accessToken = data.access_token;
+        }, 
+        (error: HttpErrorResponse) => {
+          this.success = false;
+          this.error = true;
+          this.errorMsg = error.error.message;
+        }
+      );
   }
 
 }
