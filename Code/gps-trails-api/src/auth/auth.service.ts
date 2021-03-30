915c113265c1ef619f1b67f2828f173173496d9f
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-03-29 23:09:15 -0500

    Return user object with access_token

--- Code/gps-trails-api/src/auth/auth.service.ts
+++ Code/gps-trails-api/src/auth/auth.service.ts
@@ -26,6 +26,11 @@ export class AuthService {
         const payload = { email: user.email, sub: user.id };
         return {
             access_token: this.jwtService.sign(payload),
+            user: {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+            },
         };
     }
 }
