c66aa6a762244ab67742007190e734dcb246377a
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-03-30 11:27:44 -0500

    Update state on login

--- Code/gps-trails/src/app/account/login/login.component.ts
+++ Code/gps-trails/src/app/account/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { loggedIn } from '../../reducers/auth.reducer';
+import { Observable } from 'rxjs';
+import { login } from 'src/app/actions/auth.action';
 
 @Component({
   selector: 'app-login',
@@ -11,19 +15,20 @@ export class LoginComponent implements OnInit {
 
   email: string = "";
   password: string = "";
-
   errorMsg: string = "";
-
   user: any = null;
   accessToken: string = "";
-
   error: boolean = false;
   success: boolean = false;
+  loggedIn$: Observable<boolean>;
 
   constructor(
     private httpClient: HttpClient,
-    private router: Router
-  ) {}
+    private router: Router,
+    private store: Store<{ loggedIn: boolean }>,
+  ) {
+    this.loggedIn$ = store.select('loggedIn');
+  }
 
   ngOnInit(): void {
     if (localStorage.getItem('access_token') && localStorage.getItem('user')) {
@@ -50,6 +55,8 @@ export class LoginComponent implements OnInit {
           
           localStorage.setItem('access_token', this.accessToken);
           localStorage.setItem('user', JSON.stringify(this.user));
+
+          this.store.dispatch(login());
         }, 
         (error: HttpErrorResponse) => {
           this.success = false;
