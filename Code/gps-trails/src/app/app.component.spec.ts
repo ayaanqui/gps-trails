768d8dd9dfd65ca0c7e097640b093716547d94db
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-04-23 22:27:25 -0500

    Fix more tests

--- Code/gps-trails/src/app/app.component.spec.ts
+++ Code/gps-trails/src/app/app.component.spec.ts
@@ -1,35 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Store } from '@ngrx/store';
 import { AppComponent } from './app.component';
+import { AuthStatus, status } from './reducers/auth.reducer';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule,],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'gps-trails'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('gps-trails');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('gps-trails app is running!');
   });
 });
--- Code/gps-trails/src/app/explore/explore.component.spec.ts
+++ Code/gps-trails/src/app/explore/explore.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExploreComponent } from './explore.component';
@@ -8,7 +9,8 @@ describe('ExploreComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ExploreComponent ]
+      declarations: [ExploreComponent],
+      imports: [HttpClientModule]
     })
     .compileComponents();
   });
