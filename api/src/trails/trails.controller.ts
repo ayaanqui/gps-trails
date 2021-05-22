22a8c2e656a8b6756665bbc7eb448b6aa69bae79
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-05-22 16:41:16 -0400

    New controller

--- api/src/trails/trails.controller.ts
+++ api/src/trails/trails.controller.ts
@@ -1,7 +1,5 @@
 import { Body, Controller, Get, HttpException, HttpStatus, Param, Post, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
-import { Trail as OldTrail } from "./trail";
-import { data } from "./trails.json";
 import { CreateTrailDto } from './dto/create-trail-dto';
 import { TrailsService } from "./trails.service";
 import { Trail } from "./trails.entity";
@@ -9,40 +7,33 @@ import { Trail } from "./trails.entity";
 @Controller('trails')
 export class TrailsController {
 
-  trails: Array<OldTrail> = new Array<OldTrail>();
-  nameToTrailMap: Map<string, OldTrail> = new Map<string, OldTrail>();
-
   constructor(private readonly trailServices: TrailsService) {
-    data.forEach((t, i) => {
-      let trailOb = new OldTrail(i, t.name, t.image, t.description, t.latitude, t.longitude, t.parkarea, t.contact, t.subtrails, t.ratingsAvg, t.views);
-
-      this.trails.push(trailOb);
-      this.nameToTrailMap.set(trailOb.name, trailOb);
-    });
   }
 
   @Get()
-  getAllTrails(): OldTrail[] {
-    return this.trails;
+  async getAllTrails(): Promise<Trail[]> {
+    return await this.trailServices.findAll();
   }
 
   @Get(':id')
-  getTrailById(@Param() params): OldTrail {
+  async getTrailById(@Param() params): Promise<Trail> {
     const id: number = params?.id;
-    if (id == null || id >= this.trails.length)
-      throw new HttpException({
-        status: HttpStatus.NOT_FOUND,
-        error: 'Trail not found',
-      },
-        HttpStatus.NOT_FOUND);
-    return this.trails[id];
+    const post: Trail = await this.trailServices.findById(id);
+    if (id == null || post == null)
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: 'Trail not found',
+        },
+        HttpStatus.NOT_FOUND
+      );
+    return post;
   }
 
   @UseGuards(JwtAuthGuard)
   @Post()
   async createNewTrail(@Body() trailData: CreateTrailDto): Promise<Trail> {
-    const newTrail: Trail = await this.trailServices.insert(trailData);
-    return newTrail;
+    return await this.trailServices.insert(trailData);
   }
 
 }
