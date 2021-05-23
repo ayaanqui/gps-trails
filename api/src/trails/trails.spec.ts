2356e2c9b93b3ad2681134e269e9048ebcb03478
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-05-23 18:32:17 -0400

    Remove old invalid tests

--- api/src/trails/trails.spec.ts
+++ api/src/trails/trails.spec.ts
@@ -1,5 +1,4 @@
 import { Test } from '@nestjs/testing';
-import { Trail } from './trail';
 import { Trail as Trails } from './trails.entity';
 
 describe('Trails test', () => {
@@ -11,23 +10,8 @@ describe('Trails test', () => {
   });
 });
 
-describe('Trail.genRandom test', () => {
+/* describe('Trail.genRandom test', () => {
   it('should return random number', async () => {
-    for(var i = 0; i < 100000; i++){
-      expect(Trail.genRandom(0, 10) == Trail.genRandom(11, 20)).toBe(false);
-      expect(Trail.genRandom(-54154, 1541) < Trail.genRandom(1542, 45135)).toBe(true);
-      expect(Trail.genRandom(54654, 456546) > Trail.genRandom(0, 41561)).toBe(true);
-    }
+    // Your test here...
   });
-});
-
-
-describe('Trail.genRandomDec test', () => {
-  it('should return random number', async () => {
-    for(var i = 0; i < 100000; i++){
-      expect(Trail.genRandomDec(0, 10, 2) == Trail.genRandomDec(11, 20, 2)).toBe(false);
-      expect(Trail.genRandomDec(-3451, 20, 2) < Trail.genRandomDec(21, 5125, 3)).toBe(true);
-      expect(Trail.genRandomDec(4848, 74515, 3) > Trail.genRandomDec(-15162, 4847, 3)).toBe(true);
-    }
-  });
-});
+}); */
