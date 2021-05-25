46eb5819503c61459f15817c255ac8d31ad7e2e0
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-05-24 20:26:00 -0400

    Non-blocking code, let's go!!

--- api/src/trails/trails.controller.ts
+++ api/src/trails/trails.controller.ts
@@ -2,33 +2,35 @@ import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common'
 import { TrailsService } from './trails.service';
 import { CreateTrailDto } from './dto/create-trail.dto';
 import { UpdateTrailDto } from './dto/update-trail.dto';
+import { Trail } from './entities/trail.entity';
+import { DeleteResult, UpdateResult } from 'typeorm';
 
 @Controller('trails')
 export class TrailsController {
   constructor(private readonly trailsService: TrailsService) {}
 
   @Post()
-  create(@Body() createTrailDto: CreateTrailDto) {
-    return this.trailsService.create(createTrailDto);
+  async create(@Body() createTrailDto: CreateTrailDto): Promise<Trail> {
+    return await this.trailsService.create(createTrailDto);
   }
 
   @Get()
-  findAll() {
-    return this.trailsService.findAll();
+  async findAll(): Promise<Trail[]> {
+    return await this.trailsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.trailsService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<Trail> {
+    return await this.trailsService.findOne(+id);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateTrailDto: UpdateTrailDto) {
-    return this.trailsService.update(+id, updateTrailDto);
+  async update(@Param('id') id: string, @Body() updateTrailDto: UpdateTrailDto): Promise<UpdateResult> {
+    return await this.trailsService.update(+id, updateTrailDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.trailsService.remove(+id);
+  async remove(@Param('id') id: string): Promise<DeleteResult> {
+    return await this.trailsService.remove(+id);
   }
 }
