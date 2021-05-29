af36fee7c50f011b278a9a291cd6b6d3b0ae1427
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-05-28 21:05:55 -0500

    Parse data table

--- park-data-crawler/index.js
+++ park-data-crawler/index.js
@@ -1,4 +1,9 @@
+const url = 'https://en.wikipedia.org/wiki/List_of_national_parks_of_the_United_States';
 const request = require('request');
-const page = request('https://en.wikipedia.org/wiki/List_of_national_parks_of_the_United_States', (err, res, body) => {
-  console.log(body);
+const page = request(url, (err, res, body) => {
+  const cheerio = require('cheerio');
+  const $ = cheerio.load(body);
+
+  const items = $('.mw-parser-output table.wikitable').html();
+  console.log(items);
 });
