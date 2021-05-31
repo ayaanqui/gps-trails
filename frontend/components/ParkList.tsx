1baab779417ccd595e8c35ad31e561157fbfc06a
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-05-31 17:57:19 -0500

    Implement ratings

--- frontend/components/ParkList.tsx
+++ frontend/components/ParkList.tsx
@@ -1,11 +1,54 @@
-import { Heading, Box, Text, Flex, Image, Link } from '@chakra-ui/react';
+import {
+  Heading,
+  Box,
+  Text,
+  Flex,
+  Image,
+  Link,
+  Icon,
+} from '@chakra-ui/react';
 import Park from "../types/Park";
 import api from '../util/api';
+import { BsStar, BsStarFill, BsStarHalf } from 'react-icons/bs';
+import { IconType } from 'react-icons/lib';
+
+const starElement = (icon: IconType) => (
+  <Icon
+    as={icon}
+    mr='0.5'
+    color='yellow.400'
+  />
+);
+
+const getStars = (ratingsAvg: number) => {
+  const stars: any[] = [];
+  const ratings: number = ratingsAvg;
+  let leftovers: number = 5 - ratings;
+
+  let r = ratings;
+
+  for (let i = 0; i < Math.floor(ratings); i++) {
+    stars.push(starElement(BsStarFill));
+    r -= 1;
+  }
+
+  // Add half star if r is not 0
+  if (r > 0) {
+    stars.push(starElement(BsStarHalf));
+    leftovers--;
+  }
+
+  // Add left over empty stars
+  for (let i = 0; i < leftovers; i++)
+    stars.push(starElement(BsStar));
+  return stars;
+};
+
+const numFormat = (num: number): string => {
+  return new Intl.NumberFormat('en-US', { maximumSignificantDigits: 10 }).format(num)
+}
 
 export default function ParkList({ parks }: { parks: Array<Park> }) {
-  const numFormat = (num: number): string => {
-    return new Intl.NumberFormat('en-US', { maximumSignificantDigits: 3 }).format(num)
-  }
 
   return (
     <>
@@ -32,17 +75,18 @@ export default function ParkList({ parks }: { parks: Array<Park> }) {
               <Box
                 flex='2'
                 pl='4' pr='4'
-                pt='3' pb='3'
+                pt='1' pb='1'
               >
                 <Heading size='lg'>
                   <Link href={`/park/${park.id}`}>
                     {park.name}
                   </Link>
                 </Heading>
+
                 <Flex
                   size='xs'
                   color='gray.500'
-                  mb='2'
+                  mb='1'
                   direction='row'
                   justifyContent='space-between'
                 >
@@ -50,6 +94,14 @@ export default function ParkList({ parks }: { parks: Array<Park> }) {
 
                   <Text>{numFormat(park.parkArea)} acres</Text>
                 </Flex>
+
+                <Flex mb='3'>
+                  {getStars(park.ratingsAvg).map(star => star)}
+                  <Text color='gray.600'>
+                    ({park.ratingsAvg})
+                  </Text>
+                </Flex>
+
                 <Text noOfLines={3}>{park.description}</Text>
               </Box>
             </Flex>
