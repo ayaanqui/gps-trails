051e4db7bd4b041373328b50f060ae24ab337cf5
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-02 12:56:15 -0500

    Dispatch login on application

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,8 +3,14 @@ import { ChakraProvider, Container } from '@chakra-ui/react'
 import Navbar from '../components/Navbar'
 
 import '../styles/globals.css'
+import { login, authStore } from '../store/authStore';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    authStore.dispatch(login())
+  })
+
   return (
     <ChakraProvider>
       <Container h='50px' maxW='full'>
