97b666436720d6104b0a807b3b222ad1ca873173
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-02 19:35:54 -0500

    Add iconSize param

--- frontend/util/stars.tsx
+++ frontend/util/stars.tsx
@@ -3,16 +3,30 @@ import {
 } from '@chakra-ui/react';
 import { BsStar, BsStarFill, BsStarHalf } from 'react-icons/bs';
 import { IconType } from 'react-icons/lib';
+import { Token } from '@chakra-ui/styled-system/dist/types/utils';
+import * as CSS from "csstype";
 
-export const starElement = (icon: IconType) => (
-  <Icon
-    as={icon}
-    mr='0.5'
-    color='yellow.400'
-  />
+export const starElement = (icon: IconType, iconSize?: Token<CSS.Property.Width | number, "sizes">) => (
+  <>
+    {iconSize ? (
+      <Icon
+        as={icon}
+        mr='0.5'
+        color='yellow.400'
+        boxSize={iconSize}
+      />
+    ) : (
+      <Icon
+        as={icon}
+        mr='0.5'
+        color='yellow.400'
+        boxSize=''
+      />
+    )}
+  </>
 );
 
-export const getStars = (ratingsAvg: number) => {
+export const getStars = (ratingsAvg: number, iconSize?: Token<CSS.Property.Width | number, "sizes">) => {
   const stars: any[] = [];
   const ratings: number = ratingsAvg;
   let leftovers: number = 5 - ratings;
@@ -20,18 +34,18 @@ export const getStars = (ratingsAvg: number) => {
   let r = ratings;
 
   for (let i = 0; i < Math.floor(ratings); i++) {
-    stars.push(starElement(BsStarFill));
+    stars.push(starElement(BsStarFill, iconSize));
     r -= 1;
   }
 
   // Add half star if r is not 0
   if (r > 0) {
-    stars.push(starElement(BsStarHalf));
+    stars.push(starElement(BsStarHalf, iconSize));
     leftovers--;
   }
 
   // Add left over empty stars
   for (let i = 0; i < leftovers; i++)
-    stars.push(starElement(BsStar));
+    stars.push(starElement(BsStar, iconSize));
   return stars;
 };
