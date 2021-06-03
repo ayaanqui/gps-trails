45c350e1d00238fa1f5507f77772636959d5ce39
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-02 21:22:04 -0500

    Create ParkDetailed component

---
+++ frontend/components/ParkDetailed.tsx
@@ -0,0 +1,55 @@
+import {
+  Heading,
+  Flex,
+  Image,
+  Text,
+  Box,
+  Icon,
+} from '@chakra-ui/react'
+import api from '../util/api'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import { FaMapMarkerAlt } from 'react-icons/fa'
+import { getStars } from '../util/stars'
+import { numFormat } from '../util/formatters'
+import Park from '../types/Park'
+
+export default function ParkDetailed({ park }: { park: Park }) {
+  return (
+    <>
+      <Image w='full' src={`${api.static}${park.image}`} />
+
+      <Box p='3' mb='7'>
+        <Heading mb='2'>{park.name}</Heading>
+        <Flex
+          size='xs'
+          color='gray.500'
+          mb='2'
+          direction='row'
+          justifyContent='space-between'
+        >
+          <Text mr='4' display='flex' alignItems='center'>
+            <Icon
+              as={FaMapMarkerAlt}
+              mr='1'
+            />
+            {park.location}
+          </Text>
+
+          <Text>{numFormat(park.parkArea)} acres</Text>
+        </Flex>
+        <Box mb='7'>
+          {getStars(park.ratingsAvg, '6').map(star => star)}
+        </Box>
+        <Text>
+          {park.description}
+        </Text>
+      </Box>
+
+      <Box p='3' mb='7'>
+        <Heading size='md'>
+          Reviews (11)
+        </Heading>
+      </Box>
+    </>
+  )
+}
