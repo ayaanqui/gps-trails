4d94e18dc781f6c0adb702ccc9031245af62032d
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-05 23:29:58 -0500

    Implement /reviews controller

--- api/src/reviews/reviews.controller.ts
+++ api/src/reviews/reviews.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, UseGuards, Request, HttpException, HttpStatus } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Review } from 'src/reviews/entities/review.entity';
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createReviewDto: CreateReviewDto) {
-    return this.reviewsService.create(createReviewDto);
+  async create(
+    @Request() req,
+    @Body() createReviewDto: CreateReviewDto
+  ) {
+    const user = req.user;
+    const newReview = await this.reviewsService.create(createReviewDto, user);
+    return newReview;
   }
 
   @Get()
@@ -17,18 +25,8 @@ export class ReviewsController {
     return this.reviewsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reviewsService.findOne(+id);
-  }
-
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateReviewDto: UpdateReviewDto) {
-    return this.reviewsService.update(+id, updateReviewDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reviewsService.remove(+id);
+  @Get(':parkId')
+  async findParkReview(@Param('parkId') parkId: number): Promise<Review[]> {
+    return await this.reviewsService.findByParkId(parkId);
   }
 }
