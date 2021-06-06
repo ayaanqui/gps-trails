888446e7f67841aa5f8bab637b66af8dbbe1d6d1
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-05 23:29:32 -0500

    Add review services

--- api/src/reviews/reviews.service.ts
+++ api/src/reviews/reviews.service.ts
@@ -1,26 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ParksService } from 'src/parks/parks.service';
+import { User } from 'src/users/users.entity';
+import { Repository } from 'typeorm';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { Review } from './entities/review.entity';
 
 @Injectable()
 export class ReviewsService {
-  create(createReviewDto: CreateReviewDto) {
-    return 'This action adds a new review';
+  constructor(
+    @InjectRepository(Review)
+    private readonly reviewsRepository: Repository<Review>,
+    private readonly parksService: ParksService,
+  ) { }
+
+  async create(createReviewDto: CreateReviewDto, user: User): Promise<Review> {
+    // Fetch park...
+    const park = await this.parksService.findById(createReviewDto.parkId);
+    if (!park)
+      throw new HttpException({
+        message: 'Invalid park'
+      }, HttpStatus.BAD_REQUEST)
+
+    const review = new Review();
+    review.park = park;
+    review.rating = createReviewDto.rating;
+    review.review = createReviewDto.review;
+    review.user = user;
+
+    return await review.save();
   }
 
-  findAll() {
-    return `This action returns all reviews`;
+  async findAll(): Promise<Review[]> {
+    return await this.reviewsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} review`;
-  }
-
-  update(id: number, updateReviewDto: UpdateReviewDto) {
-    return `This action updates a #${id} review`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} review`;
+  async findByParkId(parkId: number): Promise<Review[]> {
+    const park = await this.parksService.findById(parkId);
+    if (!park)
+      throw new HttpException({
+        message: 'Invalid park'
+      }, HttpStatus.BAD_REQUEST);
+    return await this.reviewsRepository.find({
+      relations: ['user'],
+      where: { park }
+    })
   }
 }
