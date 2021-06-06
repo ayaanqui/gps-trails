2763dd0bf9088fe6a2931d5f0be9ca2a16635ad9
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-06 14:58:48 -0500

    API: join average park rating from Review table into Park.ratingsAvg

--- api/src/parks/parks.service.ts
+++ api/src/parks/parks.service.ts
@@ -5,13 +5,32 @@ import { Park } from "./parks.entity";
 
 @Injectable()
 export class ParksService {
+  private static readonly selectAllWithRating = [
+    'parks.id AS id',
+    'parks.name AS name',
+    'parks.description AS description',
+    'parks.image AS image',
+    'parks.lat AS lat',
+    'parks.lon AS lon',
+    'parks.location AS location',
+    'parks.contact AS contact',
+    'parks.views AS views',
+    'parks.parkArea AS parkArea',
+    'round(coalesce(avg(review.rating), 0), 1) AS ratingsAvg'
+  ];
+
   constructor(
     @InjectRepository(Park)
     private readonly parkRepository: Repository<Park>,
   ) { }
 
   async findById(id: number): Promise<Park> {
-    return await this.parkRepository.findOne(id);
+    return await this.parkRepository
+      .createQueryBuilder('parks')
+      .select(ParksService.selectAllWithRating)
+      .leftJoin('parks.reviews', 'review')
+      .where('parks.id = :parkId', { parkId: id })
+      .getRawOne();
   }
 
   async insert(park: Park): Promise<Park> {
@@ -25,10 +44,12 @@ export class ParksService {
   async findAllWithLimit(limit: number, offset: number): Promise<Park[]> {
     return await this.parkRepository
       .createQueryBuilder('parks')
-      .select()
+      .select(ParksService.selectAllWithRating)
+      .leftJoin('parks.reviews', 'review')
+      .groupBy('parks.id')
       .limit(limit)
       .offset(offset)
-      .getMany();
+      .getRawMany();
   }
 
   async searchByNameDescriptionLocation(q: string): Promise<Park[]> {
